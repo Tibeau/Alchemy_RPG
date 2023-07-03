9820366ef00881dbf7aad7a0cdc55b3ba75b4587
unknown <Tibeau.vandenbroeck25@gmail.com>
2023-07-03 18:07:05 +0200

    ARPG add shoot functionallity and aim

---
+++ Assets/Bullet/Bullet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        switch(other.gameObject.tag)
+        {
+            case "Wall":
+            Destroy(gameObject);
+            break;
+            case "Enemy":
+            // other.GameObject.GetComponent<MyEnemyScript>().takeDamage();
+             Destroy(gameObject);
+            break;
+        
+        }
+    }
+}
