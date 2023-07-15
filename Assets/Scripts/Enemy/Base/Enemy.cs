7f48a781df04370d72c3c8b2bd89e780959d33c5
unknown <Tibeau.vandenbroeck25@gmail.com>
2023-07-15 23:03:25 +0200

    Add statemachine

---
+++ Assets/Scripts/Enemy/Base/Enemy.cs
@@ -0,0 +1,124 @@
+
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, IDamageable, IEnemyMovable, ITriggerCheckable
+{
+    [field: SerializeField] public float MaxHealth { get; set; } = 100f;
+    public float CurrentHealth { get; set; }
+    public Rigidbody2D RB { get; set; }
+    public bool IsFacingRight { get; set; } = true;
+
+    public bool IsAggroed { get; set; }
+    public bool IsWithinStrikingDistance { get; set; }
+
+    #region State Machine Variables
+
+    #region Idle Variables
+
+    public Rigidbody2D BulletPefab;
+    public float movementRange = 5f;
+    public float movementSpeed = 1f;
+    #endregion
+
+    public EnemyStateMachine StateMachine {get; set;}
+    public EnemyIdleState IdleState {get; set;}
+    public EnemyAttackState AttackState {get; set;}
+    public EnemyChaseState ChaseState {get; set;}
+    #endregion
+
+
+    private void Awake() {
+        StateMachine = new EnemyStateMachine();
+        IdleState = new EnemyIdleState(this, StateMachine);
+        AttackState = new EnemyAttackState(this, StateMachine);
+        ChaseState = new EnemyChaseState(this, StateMachine);
+    }
+
+    private void Start()
+    {
+        CurrentHealth = MaxHealth;
+        RB = GetComponent<Rigidbody2D>();
+        StateMachine.Initialize(IdleState);
+    }
+
+    private void Update() {
+        StateMachine.CurrentEnemyState.FrameUpdate();
+    }
+
+    private void FixedUpdate() {
+        StateMachine.CurrentEnemyState.PhysicsUpdate();
+    }
+
+    #region Health / Die Functions
+
+    public void Damage(float damageAmount)
+    {
+        CurrentHealth -= damageAmount;
+
+        if (CurrentHealth <= 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    #endregion
+
+
+    #region Movement Functions
+
+    public void MoveEnemy(Vector2 velocity)
+    {
+        RB.velocity = velocity;
+        CheckForLeftOrRightFacing(velocity);
+    }
+
+    public void CheckForLeftOrRightFacing(Vector2 velocity)
+    {
+        if (IsFacingRight && velocity.x < 0f) {
+            Vector3 rotator = new Vector3(transform.rotation.x, 180f, transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotator);
+            IsFacingRight = !IsFacingRight;
+        } else if (!IsFacingRight && velocity.x > 0f) {
+            Vector3 rotator = new Vector3(transform.rotation.x, 0f, transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotator);
+            IsFacingRight = !IsFacingRight;
+        }
+    }
+
+    #endregion
+
+#region Animation Triggers
+
+private void AnimationTriggerEvent(AnimationTriggerType triggerType) {
+    StateMachine.CurrentEnemyState.AnimationTriggerEvent(triggerType);
+}
+
+    public enum AnimationTriggerType {
+    EnemyDamaged
+}
+
+
+#endregion
+
+
+#region Distance Checks
+
+    public void SetAggroStatus(bool isAggroed)
+    {
+        IsAggroed = isAggroed;
+    }
+
+    public void SetStrikingDistanceBool(bool isWithinStrikingDistance)
+    {
+       IsWithinStrikingDistance = isWithinStrikingDistance;
+    }
+
+#endregion
+
+}
+
---
+++ Assets/Scripts/Enemy/Interfaces/IDamageable.cs
@@ -0,0 +1,11 @@
+
+public interface IDamageable 
+{
+   void Damage(float damageAmount);
+
+   void Die();
+
+   float MaxHealth { get; set; }
+
+   float CurrentHealth { get; set;}
+}
